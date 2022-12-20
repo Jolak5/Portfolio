9a81eafd6b691fcc7a07d764327ef3ca4f9df2e8
jolak5 <kayodej4@gmail.com>
2022-12-20 14:28:53 +0100

    fix linter errors

--- index.js
+++ index.js
@@ -1,16 +1,2 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
-const body = document.querySelector("body");
-const navItem = document.querySelectorAll(".nav-item");
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-  body.style.overflow = "hidden";
-});
-
-for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener("click", function () {
-    navMenu.classList.remove("active");
-    hamburger.classList.remove("active");
-  });
+const hamburger = document.querySelector('.hamburger'); const navMenu = document.querySelector('.nav-menu'); const body = document.querySelector('body'); const navItem = document.querySelectorAll('.nav-item'); hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); navMenu.classList.toggle('active'); body.style.overflow = 'hidden'; }); for (let i = 0; i < navItem.length; i++) { navItem[i].addEventListener('click', () => { navMenu.classList.remove('active'); hamburger.classList.remove('active'); });
 }
