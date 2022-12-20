acd554373aafa4591f5755d1706a931500ab3379
jolak5 <kayodej4@gmail.com>
2022-12-20 14:55:15 +0100

    fix linters in javascript

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const hamburger = document.querySelector('.hamburger'); const navMenu = document.querySelector('.nav-menu'); const body = document.querySelector('body'); const navItem = document.querySelectorAll('.nav-item'); hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); navMenu.classList.toggle('active'); body.style.overflow = 'hidden'; }); for (let i = 0; i < navItem.length; i++) { navItem[i].addEventListener('click', () => { navMenu.classList.remove('active'); hamburger.classList.remove('active'); });
+/* eslint-disable no-plusplus */
+const hamburger = document.querySelector('.hamburger'); const navMenu = document.querySelector('.nav-menu'); const body = document.querySelector('body'); const navItem = document.querySelectorAll('.nav-item'); hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); navMenu.classList.toggle('active'); body.style.overflow = 'hidden'; }); for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener('click', () => { navMenu.classList.remove('active'); hamburger.classList.remove('active'); });
 }
