7e53eeeaef6edf4517a4710ce6605193ef98bd69
jolak5 <kayodej4@gmail.com>
2022-12-21 11:52:06 +0100

    Change the Javascript and HTML to toggle hamburger

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 /* eslint-disable no-plusplus */
-const hamburger = document.querySelector('.hamburger'); const navMenu = document.querySelector('.nav-menu'); const body = document.querySelector('body'); const navItem = document.querySelectorAll('.nav-item'); hamburger.addEventListener('click', () => { hamburger.classList.toggle('active'); navMenu.classList.toggle('active'); body.style.overflow = 'hidden'; }); for (let i = 0; i < navItem.length; i++) {
-  navItem[i].addEventListener('click', () => { navMenu.classList.remove('active'); hamburger.classList.remove('active'); });
+const hamburger = document.querySelector('.hamburger'); const navMenu = document.querySelector('.nav-menu'); const welcome = document.querySelector('.welcome'); const body = document.querySelector('body'); const navItem = document.querySelectorAll('.nav-item');
+const closeIcon =document.querySelector('.close_icon')
+
+hamburger.addEventListener('click', () => { 
+  hamburger.classList.toggle('active');      
+navMenu.classList.toggle('active'); 
+welcome.classList.toggle('active');
+body.style.overflow = 'hidden'; 
+}); 
+
+for (let i = 0; i < navItem.length; i++) { 
+  navItem[i].addEventListener('click', () => { navMenu.classList.remove('active'); hamburger.classList.toggle('active'); welcome.classList.remove('active'); body.style.overflow = 'visible'; });
 }
+
+document.querySelectorAll(".close-icon").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  welcome.classList.remove('active'); body.style.overflow = 'visible'; 
+}))
